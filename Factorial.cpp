29c11af24bad64e8fb00839a7205cfee48783cbc
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-03 15:22:58 +0600

    create file for solve Problem

---
+++ Factorial.cpp
@@ -0,0 +1,13 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [ForLoop]
+//               Problem : 👉👉 [5]
+//               Want    : Factorial
+//                        
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    
+    return 0 ;
+}
