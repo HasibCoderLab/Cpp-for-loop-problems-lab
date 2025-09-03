8c8e3a4fa4dd1e5ea2d7edb3c764f45cc6244616
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-03 14:21:37 +0600

    solve

---
+++ EvenNum1To100.cpp
@@ -0,0 +1,24 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [ForLoop]
+//               Problem : 👉👉 [2]
+//               Want    : EvenNum1To100;
+//                        
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cout<<"Enter Number " ;
+    // cin>> n ;
+    for(int  i = 1 ; i<=100; i++){
+        if(i % 2 == 0){
+     cout<<"Even Number is " <<  i <<endl;
+    }else{
+        cout<<"odd Number  " <<endl;
+
+        }
+
+    }
+    return 0 ;
+}
