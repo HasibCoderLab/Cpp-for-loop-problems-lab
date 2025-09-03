6b7e635e1455d7193e5d7275006837ca6b51b224
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-03 22:12:26 +0600

    solve

--- PrimeCheck.cpp
+++ PrimeCheck.cpp
@@ -1,15 +1,32 @@
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [ForLoop]
-//               Problem : 👉👉 [6]
+//               Problem : 👉👉 [7]
 //               Want    : DigitSum
-//                        
+//
 // ================================================
 #include <iostream>
 using namespace std;
-int main(){
-    int n ; 
-    
-    
-    return 0 ;
+int main()
+{
+    int n;
+    cout << "Enter Number ";
+    cin >> n;
+    if (n <= 1)
+    {
+        cout << n << "is Not Prime Number " << endl;
+        return 0;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << n << "is Not Prime Number " << endl;
+            return 0;
+        }
+    }
+
+    cout << n << "is  Prime Number " << endl;
+
+    return 0;
 }
