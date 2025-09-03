a7336827a996ae2fdf236c568ace6f08f3fad555
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-03 21:46:06 +0600

    create a file for solve problem

---
+++ PrimeCheck.cpp
@@ -0,0 +1,15 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [ForLoop]
+//               Problem : 👉👉 [6]
+//               Want    : DigitSum
+//                        
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int n ; 
+    
+    
+    return 0 ;
+}
