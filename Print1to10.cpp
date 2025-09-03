df039b5fcef4b4dc80f42edfde057404f178feff
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-03 14:49:48 +0600

    solve problem

--- Print1to10.cpp
+++ Print1to10.cpp
@@ -2,6 +2,15 @@
 //               Language: C++ / Cpp
 //               Topic   : [ForLoop]
 //               Problem : 👉👉 [2]
-//               Want    : Print numbers from 1 to 10
-//                        
+//               Want    : Print numbers from 1 to 10                       
 // ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 1 ; i <=10; i++)
+    {
+        cout<< i <<endl;
+    }
+    
+    return 0 ;
+}
