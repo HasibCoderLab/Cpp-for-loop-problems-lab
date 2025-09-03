c781e8a2fa64b332af922061f22de9392c846bc1
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-03 15:26:34 +0600

    solve

--- Factorial.cpp
+++ Factorial.cpp
@@ -8,6 +8,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int n;
+    cout<<"Enter Number " ; 
+    cin>> n;
+    int fact = 1;
+    for(int i = 1 ; i<=n ; i++){
+        fact  *= i ;
+        cout<< fact;
+    }
+
     return 0 ;
 }
