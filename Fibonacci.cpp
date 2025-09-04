b1c8e861f403547126dbf2cfa308772337815082
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 10:46:45 +0600

    solve

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -2,7 +2,7 @@
 //               Language: C++ / Cpp
 //               Topic   : [ForLoop]
 //               Problem : 👉👉 [7]
-//               Want    : DigitSum
+//               Want    : Fibonacci
 //
 // ================================================
 #include <iostream>
@@ -12,6 +12,14 @@ int main()
     int n;
     cout << "Enter Number ";
     cin >> n;
+    int first = 0, second = 1;
+    for(int i  = 1 ; i <=n ; i++){
+        cout << first  <<" "  <<endl;
+         int next = first + second;
+        first = second;
+        second= next;
+   cout << second  <<endl;
+    }
 
     return 0;
 }
