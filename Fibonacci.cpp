130351a0a4a1d6966aeb44829b0bae4fe6449d05
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 10:37:07 +0600

    create a file for solve problem

---
+++ Fibonacci.cpp
@@ -0,0 +1,17 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [ForLoop]
+//               Problem : 👉👉 [7]
+//               Want    : DigitSum
+//
+// ================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Number ";
+    cin >> n;
+
+    return 0;
+}
