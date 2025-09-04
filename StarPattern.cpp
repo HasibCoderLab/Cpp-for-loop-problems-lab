6bc33315a3f9d2ebbf65d665e63165e5f6bf04ef
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 11:05:39 +0600

    solve

--- StarPattern.cpp
+++ StarPattern.cpp
@@ -4,3 +4,22 @@
 //               Problem : 👉 Star Pattern
 //               Want    : Take input N and print star pattern
 // ================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+  
+    return 0;
+}
